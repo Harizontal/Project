99cbf4d420dbb151d3b9339e30bedd3ffcafd0ec
Alexey Machihine <alesha.machihine2005@mail.ru>
2024-06-09 19:41:34 +0300

    Добавьте файлы проекта.

---
+++ Product_library/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_library
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Weight { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class FoodProduct : Product
+    {
+        public DateTime ExpirationDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price}, {Weight}, {ExpirationDate}";
+        }
+    }
+
+    public class ElectronicsProduct : Product
+    {
+        public bool HasDryer { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price}, {Weight}, {HasDryer}";
+        }
+    }
+    public class FurnitureProduct : Product
+    {
+        public string Material { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price}, {Weight}, {Material}";
+        }
+    }
+
+}
